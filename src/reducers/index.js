b05bcbc0a6651512d86e332dee0d0bda3731a625
Justice Mubayiwa <j.mubayiwa@gmail.com>
2021-12-21 15:43:02 -0500

    combining reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,22 @@
+import { combineReducers } from "redux"
+
 const songsReducer =  () => {
     return [
-        {title: ''}
+        {title: 'Amg', duration: '2:48'},
+        {title: 'Easy Bake', duration: '4:52'},
+        {title: 'Hurricane', duration: '4:03'},
+        {title: '1942 Flows', duration: '4:43'}
     ]
 }
+
+const selectedSongReducer = (selectedSong = null, action)=>{
+    if(action.type === 'SONG_SELECTED'){
+        return action.payload
+    }
+    return selectedSong
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer 
+})
